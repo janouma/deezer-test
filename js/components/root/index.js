bca4503fb67865a825205787c741e361ad82a8e3
Judicaël Anouma <janouma@gmail.com>
2017-09-19 09:22:45 +0200

    successfully detecting scrollend

--- js/components/root/index.js
+++ js/components/root/index.js
@@ -3,12 +3,24 @@ import Search from 'components/search/index.js'
 import Grid from 'components/grid/index.js'
 import api from 'utils/api.js'
 
+const PAGE_SIZE = 25
+
 export default class Root extends React.Component {
   constructor (props) {
     super(props)
+
+    this.offset = 0
+
     this.state = {
       data: []
     }
+  }
+
+  componentDidMount () {
+    window.addEventListener(
+      'scroll',
+      () => this._detectEndScroll()
+    )
   }
 
   render () {
@@ -21,19 +33,18 @@ export default class Root extends React.Component {
   }
 
   async _search (query) {
+    const results = await api.get(`http://api.deezer.com/search/track?q=${query}&index=${this.offset}&limit=${PAGE_SIZE}`)
 
-    // DEBUG
-    console.debug('searching for:', query)
-    // END DEBUG
-
-    const results = await api.get(`http://api.deezer.com/search/track?q=${query}`)
-
-    // DEBUG
-    console.debug('results:', results)
-    // END DEBUG
-
-    this.setState({
+    this.setState ({
       data: results.data
     })
+  }
+
+  _detectEndScroll () {
+    if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+      // DEBUG
+      console.debug('end scroll detected')
+      // END DEBUG
+    }
   }
 }
