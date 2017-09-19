33b163eed1e0b208bb8616421e73d3f2c2ca287f
Judicaël Anouma <janouma@gmail.com>
2017-09-19 10:01:26 +0200

    implemented filter

--- js/components/grid/index.js
+++ js/components/grid/index.js
@@ -2,49 +2,80 @@ import React from 'react'
 
 import './style.css'
 
-export default function Grid ({data}) {
-  const header = [
-    'id',
-    'readable',
-    'title',
-    'link',
-    'duration',
-    'rank',
-    'explicit lyrics',
-    'preview',
-    'artist',
-    'album',
-    'type'
-  ]
+export default class Grid extends React.Component {
+  constructor (props) {
+    super(props)
 
-  return (
-    <table className='dzr-grid'>
-      <tr>
-        {header.map(name => (
-           <td>
-             {name}
-           </td>
-         ))}
-      </tr>
-      {data.map(row => {
-         const values = [
-           row.id,
-           row.readable ? 'yes' : 'no',
-           row.title,
-           row.link,
-           row.duration,
-           row.rank,
-           row.explicit_lyrics,
-           row.preview,
-           row.artist.name,
-           row.album.title,
-           row.type
-         ]
+    this.state = {
+      filtered: props.data
+    }
+  }
 
-         return (<tr key={row.id}>
-                  {values.map(value => (<td>{value}</td>))}
-                </tr>)
-       })}
-    </table>
-  )
+  componentWillReceiveProps (nextProps) {
+    this.setState({
+      filtered: nextProps.data
+    })
+  }
+
+  render () {
+    const header = [
+      'id',
+      'readable',
+      'title',
+      'link',
+      'duration',
+      'rank',
+      'explicit lyrics',
+      'preview',
+      'artist',
+      'album',
+      'type'
+    ]
+
+    return (
+      <div>
+        <p>
+          <input type='text' placeholder='filter' onInput={e => this._filter(e.target.value)} />
+        </p>
+        <table className='dzr-grid'>
+          <tr>
+            {header.map(name => (
+               <td>
+                 {name}
+               </td>
+             ))}
+          </tr>
+          {this.state.filtered.map(row => {
+             const values = [
+               row.id,
+               row.readable ? 'yes' : 'no',
+               row.title,
+               row.link,
+               row.duration,
+               row.rank,
+               row.explicit_lyrics,
+               row.preview,
+               row.artist.name,
+               row.album.title,
+               row.type
+             ]
+
+             return (<tr key={row.id}>
+                       {values.map(value => (<td>
+                                               {value}
+                                             </td>))}
+                     </tr>)
+           })}
+        </table>
+      </div>
+    )
+  }
+
+  _filter (keyWords) {
+    const lowcaseWords = keyWords.toLowerCase()
+
+    this.setState({
+      filtered: this.props.data.filter(row => row.title.toLowerCase().includes(lowcaseWords))
+    })
+  }
 }
--- js/components/search/index.js
+++ js/components/search/index.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 export default class Search extends React.Component {
   render () {
-    return <form onSubmit={e => this._search(e)}>
-             <input type='text' placeholder='type in key words' onInput={e => this.query = e.target.value} />
-           </form>
+    return (
+      <form onSubmit={e => this._search(e)}>
+        <p>
+          <input type='text' placeholder='type in key words' onInput={e => this.query = e.target.value} />
+        </p>
+      </form>
+    )
   }
 
   _search (e) {
